92aea904792cfc32eb6ad63fbab86e76c1b8a9cd
Jake Harding <jacob.s.harding@gmail.com>
2012-09-19 23:43:56 -0700

    Clean up code.

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-// Pen
-// ===
-// * GitHub: https://github.com/jharding/pen
+// Crayon
+// ======
+// * GitHub: https://github.com/jharding/crayon
 // * Copyright (c) 2012 Jake Harding
 // * Licensed under the MIT license.
 
+var f = require('util').format;
 var colors = require('colors');
+
+colors.setTheme({
+  info: 'blue',
+  warn: 'yellow',
+  success: 'green',
+  error: 'red'
+});
 
 var verbose = false;
 
 module.exports = {
-  verbose: function() { verbose = true; },
+  get verbose() { return verbose; },
+  set verbose(val) { verbose = !!val; },
 
-  log: function(message) { if (verbose) { console.log(message); } },
-  info: function(message) { if (verbose) { console.info(message.blue); } },
-  warn: function(message) { if (verbose) { console.warn(message.yellow); } },
-  success: function(message) { console.log(message.green); },
-  error: function(message) { console.error(message.red); }
+  log: function() {
+    verbose && console.log(f.apply(null, arguments));
+  },
+
+  info: function(format) {
+    verbose && console.info(f.apply(null, arguments).info);
+  },
+
+  warn: function(format) {
+    verbose && console.warn(f.apply(null, arguments).warn);
+  },
+
+  success: function(format) {
+    console.log(f.apply(null, arguments).success);
+  },
+
+  error: function(format) {
+    console.error(f.apply(null, arguments).error);
+  }
 };
